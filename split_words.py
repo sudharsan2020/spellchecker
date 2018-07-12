18600e3af94278b1d5a1b2b825289d34cb3067de
sudharsan2020 <tkgnambi@gmail.com>
2018-07-12 21:26:08 +0530

    Add logic for writing lines to files that differ by 1 word

--- split_words.py
+++ split_words.py
@@ -43,6 +43,7 @@ class parseTextFile:
     
     def __init__(self):
         self.my_lang_dict = {} #Dictionary to store exact word to word matches
+        self.my_issue_list = []
         self.misMatchCount = 0
         self.sourceListLen = 0
         self.targetListLen = 0
@@ -59,16 +60,16 @@ class parseTextFile:
                     wordList, correctedList = line.split("||@@||")
                     
                     #Tokenize the list elements
-                    wordList = wordList.split(" ")
+                    tmpwordList = wordList.split(" ")
                     #wordList = [re.sub('[^a-zA-Z0-9]+', '', _) for _ in wordList.split(" ")]
-                    correctedList = correctedList.split(" ")
+                    tmpcorrectedList = correctedList.split(" ")
                     #correctedList = [re.sub('[^\w\s]', '', _) for _ in correctedList.split(" ")]
-                    self.sourceListLen = len(wordList)
-                    self.targetListLen = len(correctedList)
+                    self.sourceListLen = len(tmpwordList)
+                    self.targetListLen = len(tmpcorrectedList)
                     if self.sourceListLen == self.targetListLen:
                         
                         #Frame the language dictionary
-                        for key,value in zip(wordList, correctedList):
+                        for key,value in zip(tmpwordList, tmpcorrectedList):
                             
                             #This is to assert that string with only special characters are not used
                             #It should be a combination of strings with special characters
@@ -76,10 +77,16 @@ class parseTextFile:
                             tmp_value = re.sub(r'[^\w\s]','',value)
                             if (key != value) and len(tmp_value.strip()) > 0:
                                 addToDict(self.my_lang_dict, key.strip(), value.strip())
-                                
+
+                    # Write lines to file that differ by one word
+                    elif self.sourceListLen + 1 == self.targetListLen:
+
+                        self.my_issue_list.append(wordList)
+                        self.my_issue_list.append(correctedList)
+
            
         #print ("Total number of mismatched lines:", self.misMatchCount)      
-        return self.my_lang_dict
+        return self.my_lang_dict, self.my_issue_list
  
 def printDict(myDict)    :
     for k,v in myDict.items():
@@ -116,11 +123,13 @@ if __name__ == "__main__":
     # Read the files sequentially
     for file in text_files:
           
-        my_dict = pt.readFromFile(file)
+        my_dict,my_issue_list = pt.readFromFile(file)
 
     #Write the contents to file
     writeToFile("wrong_words.txt", list(my_dict.keys()))
     writeToFile("correct_words.txt", list(my_dict.values()))
+
+    writeToFile("files_differ_by_1_word.txt", my_issue_list)
 
     #Print the result
     runTime = int(time.time() - startTime)
